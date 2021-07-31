1a4fdad032b54cab73a01fd7008166f49e21a7a9
devalade <74435372+devalade@users.noreply.github.com>
2021-07-31 16:00:53 +0100

    fist commit

---
+++ src/Entity/Pays.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaysRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PaysRepository::class)
+ */
+class Pays
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $codePays;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbhabitant;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCodePays(): ?string
+    {
+        return $this->codePays;
+    }
+
+    public function setCodePays(string $codePays): self
+    {
+        $this->codePays = $codePays;
+
+        return $this;
+    }
+
+    public function getNbhabitant(): ?int
+    {
+        return $this->nbhabitant;
+    }
+
+    public function setNbhabitant(int $nbhabitant): self
+    {
+        $this->nbhabitant = $nbhabitant;
+
+        return $this;
+    }
+}
---
+++ src/Form/PaysType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Pays;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PaysType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('codePays', TextType::class)
+            ->add('nbhabitant', NumberType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Pays::class,
+        ]);
+    }
+}
